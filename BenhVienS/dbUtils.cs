7e34c91abdb73b8b2a5e9982d7219c85bfd29066
HaiDang3006 <haidang03298@gmail.com>
2026-01-19 08:23:51 +0700

    dbUltils

---
+++ BenhVienS/dbUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenhVienS
+{
+    public class dbUtils
+    {
+        public static string ConnectionString =
+         "Data Source=DESKTOP-2F65IMT;Initial Catalog=haidang6868;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(ConnectionString);
+        }
+    }
+}
