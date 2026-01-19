90cdb54f7d9c250dd245b823d200c43e74186c6c
Gen-gif-w <dothanhthao1889@gmail.com>
2026-01-19 19:29:53 +0700

    Revert "Merge remote-tracking branch 'origin/BichTram-01-18' into ThanhThao-01-18"
    This reverts commit 701aacf6369a97d35d27183aadfeb616aa1de06f, reversing
    changes made to 4817201612b61ef9fd276aa04c2aa22e951f7686.

--- BenhVienS/Form2.cs
+++ BenhVienS/Form2.cs
@@ -1500,21 +1500,6 @@ namespace BenhVienS
         {
 
         }
-
-        private void txtnhaphoten_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void tabPage12_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
 
