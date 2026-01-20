f803b37d9c258b93d5fa5fc312a4e448cf2d164a
loc08377 <locpvpc08377@gmail.com>
2026-01-20 09:08:11 +0700

    add folder repository

---
+++ BenhVienS/MapperSql/AppointmentMapper.cs
@@ -0,0 +1,40 @@
+﻿using BenhVienS.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace BenhVienS.Service
+{
+    public static class AppointmentMapper
+    {
+        // map dìa một model 
+        public static Appointment AppointmentToMap(SqlDataReader reader)
+        {
+            var appointment = new Appointment
+            {
+                Id = Convert.ToInt32(reader["Id"]),
+                PatientId = Convert.ToInt32(reader["PatientId"]),
+                DoctorId = Convert.ToInt32(reader["DoctorId"]),
+                WorkScheduleId = Convert.ToInt32(reader["WorkScheduleId"]),
+                DateAppointment = Convert.ToDateTime(reader["DateAppointment"]),
+                Reasion = reader["Reasion"]?.ToString(),
+                ReceptionId = Convert.ToInt32(reader["ReceptionId"]),
+                Note = reader["Note"]?.ToString(),
+                CreateDate = Convert.ToDateTime(reader["CreateDate"]),
+            };
+            appointment.SetOrderType(reader["OrderType"].ToString());
+            appointment.SetStatus(reader["Status"].ToString());
+            return appointment;
+        }
+        // map dìa một danh sách model 
+        public static List<Appointment> AppointmentListToMap(SqlDataReader reader)
+        {
+            var list = new List<Appointment>();
+            while (reader.Read())
+            {
+                list.Add(AppointmentToMap(reader));
+            }
+            return list;
+        }   
+    }
+}
---
+++ BenhVienS/Repository/AppointmentRepository.cs
@@ -0,0 +1,32 @@
+﻿using BenhVienS.Models;
+using BenhVienS.Service;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenhVienS.Repository
+{
+    static class AppointmentRepository
+    {
+        public static List<Appointment> AppointmentListByDoctor(int id)
+        {
+            string sql = @"
+                            SELECT *
+                            FROM LichHen
+                            WHERE MaBacSi = @DoctorId
+                            AND CAST(NgayHen AS DATE) = CAST(GETDATE() AS DATE)
+                        ";
+            using (SqlConnection conn = dbUtils.GetConnection())
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@DoctorId", id);
+                SqlDataReader result = cmd.ExecuteReader();
+                return AppointmentMapper.AppointmentListToMap(result);
+            }
+        }
+    }
+}
