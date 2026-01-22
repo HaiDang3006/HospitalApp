a0228f1980f1e4226bfc398541a2e0f841da97d4
nthn0334-spec <nthn0334@gmail.com>
2026-01-22 23:40:53 +0700

    huynhnhu

--- BenhVienS/ttvphi.Designer.cs
+++ BenhVienS/ttvphi.Designer.cs
@@ -30,9 +30,9 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.dgvBenhNhan = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvBenhNhan)).BeginInit();
             this.SuspendLayout();
@@ -56,25 +56,8 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(33, 27);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(622, 366);
+            this.panel1.Size = new System.Drawing.Size(902, 541);
             this.panel1.TabIndex = 2;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(83, 80);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(401, 22);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "Nhập tên, mã bệnh nhân..";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(490, 80);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "🔍";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // dgvBenhNhan
             // 
@@ -92,13 +75,30 @@
             this.dgvBenhNhan.TabIndex = 3;
             this.dgvBenhNhan.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(490, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "🔍";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(401, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Nhập tên, mã bệnh nhân..";
+            // 
             // ttvphi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.panel1);
             this.Name = "ttvphi";
-            this.Size = new System.Drawing.Size(1037, 427);
+            this.Size = new System.Drawing.Size(1037, 665);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvBenhNhan)).EndInit();
